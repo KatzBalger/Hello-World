95a5fc5312446d691bcd5b9feff0dda696e2932f
KatzBalger <37108231+KatzBalger@users.noreply.github.com>
2018-03-06 14:12:04 +0100

    Add files via upload

---
+++ PROI0/hello.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_H
+#define HELLO_H
+class hello{
+public:
+    void print();
+};
+#endif // HELLO_H
---
+++ PROI0/main.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "hello.h"
+
+using namespace std;
+
+int main()
+{
+    hello m;
+    m.print();
+
+    return 0;
+}
